201512e4b279996706f689aa581a82613ff96080
Anthony Rosario <ajrosario08@gmail.com>
2015-11-11 21:20:00 -0800

    added subview by code

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -9,7 +9,42 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            fetchImage()
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let imageData = NSData(contentsOfURL: url)
+            if imageData != nil {
+                image = UIImage(data: imageData!)
+            } else {
+                image = nil
+            }
+        }
+    
+    }
 
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get { return imageView.image}
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+    }
+    
     
     
 }
