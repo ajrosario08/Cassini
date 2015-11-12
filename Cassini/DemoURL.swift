d408860445a605b71c17b7a2cde60f3fe53871ba
Anthony Rosario <ajrosario08@gmail.com>
2015-11-11 21:26:57 -0800

    imported class DemoURL.swift

---
+++ Cassini/DemoURL.swift
@@ -0,0 +1,18 @@
+//
+//  DemoURL.swift
+//
+//
+//  Created by Anthony Rosario on 11/10/15.
+//  Copyright © 2015 Anthony Rosario. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let Stanford = NSURL(string: "http://comm.stanford.edu/wp-content/uploads/2013/01/stanford-campus.png")
+    struct NASA {
+        static let Cassini = NSURL(string: "http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg")
+        static let Earth = NSURL(string: "http://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg")
+        static let Saturn = NSURL(string: "http://www.nasa.gov/sites/default/files/saturn_collage.jpg")
+    }
+}
