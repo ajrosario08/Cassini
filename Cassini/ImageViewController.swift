3846032a9bc53938448046418d5543e505e5a82c
Anthony Rosario <ajrosario08@gmail.com>
2015-11-12 21:44:27 -0800

    made fetching over the network happen on another queue

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -21,11 +21,18 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
     
     private func fetchImage() {
         if let url = imageURL {
-            let imageData = NSData(contentsOfURL: url)
-            if imageData != nil {
-                image = UIImage(data: imageData!)
-            } else {
-                image = nil
+            let qos = Int(QOS_CLASS_USER_INITIATED.rawValue)
+            dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in
+                let imageData = NSData(contentsOfURL: url)
+                dispatch_async(dispatch_get_main_queue()) {
+                    if url == self.imageURL {
+                        if imageData != nil {
+                            self.image = UIImage(data: imageData!)
+                        } else {
+                            self.image = nil
+                        }
+                    }
+                }
             }
         }
     }
